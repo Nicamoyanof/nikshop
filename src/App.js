29dd23a8a552ba34e617837fefbe8d8795a65a98
Nicamoyanof <nicamoyanofunes98@gmail.com>
2022-02-12 12:01:12 -0300

    correcciopn img

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ export default function App() {
         <div>
             <header className="headerHome">
                 <AiOutlineMenu className='iconHamburger' onClick={()=>deployMenu()} />
-                <img src='img/logo.jpgn' className='logoHeader' />
+                <img src='img/logo.jpg' className='logoHeader' />
                 <form className='searchHeader'>
                     <input type='search' class='inputSearchHeader' placeholder='Buscar' />
                     <IoSearchOutline className='iconSearchHeader' />
