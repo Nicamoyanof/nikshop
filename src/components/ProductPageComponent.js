be729ba5984152ac2bd4e1005782cfab0ee716d5
Nicamoyanof <nicamoyanofunes98@gmail.com>
2022-02-13 17:26:50 -0300

    seguimos corrigiendo la pagina de los prodyuctos

--- src/components/ProductPageComponent.js
+++ src/components/ProductPageComponent.js
@@ -1,52 +1,73 @@
 import '../../styles/components/ProductPageComponent.css';
-import { BsCreditCard2Back, BsCash } from "react-icons/bs";
+import { BsCreditCard2Back, BsCash, BsTruck } from "react-icons/bs";
+import { GrFormAdd, GrFormSubtract } from "react-icons/gr";
 
 
 export default function ProductPageComponent() {
     return (
         <div className="containerProductPages">
             <div className="containerImages">
+                <img src="../assets/img/remera.jpg" className='imageProductPrincipal' />
                 <div className='swiperImages'>
+                    <img src="../assets/img/remera.jpg" className='imageProductPage' />
+                    <img src="../assets/img/remera.jpg" className='imageProductPage' />
+                    <img src="../assets/img/remera.jpg" className='imageProductPage' />
+                    <img src="../assets/img/remera.jpg" className='imageProductPage' />
+                    <img src="../assets/img/remera.jpg" className='imageProductPage' />
+                    <img src="../assets/img/remera.jpg" className='imageProductPage' />
                     <img src="../assets/img/remera.jpg" className='imageProductPage' />
                     <img src="../assets/img/remera.jpg" className='imageProductPage' />
                     <img src="../assets/img/remera.jpg" className='imageProductPage' />
                 </div>
                 <p className='paginationImg'>1/3</p>
             </div>
-
-
-            <span className='containerCategoryPages'><p>Inicio &gt; Remeras &gt; Remera Marca X</p></span>
-            <h1 className='titleProductPage'>Remera Marca X</h1>
-            <h3 className='priceProductPage'>$2500.00</h3>
-            <div className='payMethodsProduct'>
-                <span className='itemMethodsPay'><BsCreditCard2Back className='iconItemsPay' /> <p className='textMethodsPay'> 6 coutas sin interes de $416.66</p> </span>
-                <span className='itemMethodsPay'><BsCreditCard2Back className='iconItemsPay' /> <p className='textMethodsPay'>  6 coutas sin interes de $416.66</p></span>
-                <span className='itemMethodsPay'><BsCash className='iconItemsPay cashIcon' /> <p className='textMethodsPay'> 5% de descuento pagando con Transferencia Bancaria</p></span>
+            <div className='containerInfoProductPage'>
+                <span className='containerCategoryPages'><p>Inicio &gt; Remeras &gt; Remera Marca X</p></span>
+                <h1 className='titleProductPage'>Remera Marca X</h1>
+                <h3 className='priceProductPage'>$2500.00</h3>
+                <div className='payMethodsProduct'>
+                    <span className='itemMethodsPay'><BsCreditCard2Back className='iconItemsPay' /> <p className='textMethodsPay'> 6 coutas sin interes de $416.66</p> </span>
+                    <span className='itemMethodsPay'><BsCreditCard2Back className='iconItemsPay' /> <p className='textMethodsPay'>  6 coutas sin interes de $416.66</p></span>
+                    <span className='itemMethodsPay'><BsCash className='iconItemsPay cashIcon' /> <p className='textMethodsPay'> 5% de descuento pagando con Transferencia Bancaria</p></span>
+                </div>
+                <span className='containerColorsProduct'>
+                    <h4 className='titleColorProduct'>Color</h4>
+                    <div className='containerBoxColor'>
+                        <span className='BoxColor'></span>
+                        <span className='BoxColor'></span>
+                        <span className='BoxColor'></span>
+                    </div>
+                </span>
+                <span className='containerSizeProduct'>
+                    <h4 className='titleSizeProduct'>Talle</h4>
+                    <div className='containerBoxSize'>
+                        <span className='BoxSize'>XS</span>
+                        <span className='BoxSize'>S</span>
+                        <span className='BoxSize'>M</span>
+                        <span className='BoxSize'>L</span>
+                        <span className='BoxSize'>XL</span>
+                        <span className='BoxSize'>XXL</span>
+                        <span className='BoxSize'>XXXL</span>
+                    </div>
+                </span>
+                <div className='quintityProduct'>
+                    <span className='boxAddproduct'>
+                        <GrFormSubtract className='iconAddProducts' />
+                        <p className='quantityProductsAdd'>1</p>
+                        <GrFormAdd className='iconAddProducts' />
+                    </span>
+                    <button className='btnAddShopping'>AGREGAR AL CARRITO</button>
+                </div>
+                <div className='formShipping'>
+                    <span className='containerTextShipping'> <BsTruck className='iconTruckShipping' /> <p className='textMethodsShipping'>Medio de envio</p> </span>
+                    <form className='formAddress'>
+                        <input type="text" className='inputCodeAddress' placeholder="Tu codigo postal" />
+                        <input type="submit" className='btnSubmitAddress' value="CALCULAR" />
+                    </form>
+                    <p className='moreInfoAddress'>No se mi codigo postal</p>
+                </div>
             </div>
-            <span className='containerColorsProduct'>
-                <h4 className='titleColorProduct'>Color</h4>
-                <div className='containerBoxColor'>
-                    <span className='BoxColor'></span>
-                    <span className='BoxColor'></span>
-                    <span className='BoxColor'></span>
-                </div>
-            </span>
-            <span className='containerSizeProduct'>
-                <h4 className='titleSizeProduct'>Talle</h4>
-                <div className='containerBoxSize'>
-                    <span className='BoxSize'>XS</span>
-                    <span className='BoxSize'>S</span>
-                    <span className='BoxSize'>M</span>
-                    <span className='BoxSize'>L</span>
-                    <span className='BoxSize'>XL</span>
-                    <span className='BoxSize'>XXL</span>
-                    <span className='BoxSize'>XXXL</span>
-                </div>
-            </span>
-            <div className='quintityProduct'>Cantiddad</div>
-            <button className='btnAddShopping'>Agregar al carriot</button>
-            <span className='formShipping'>Envio</span>
-            <div className='moreInfo'>Mas info</div>
+
         </div>
     )
 }
--- src/pages/ProductPage.js
+++ src/pages/ProductPage.js
@@ -1,7 +1,11 @@
 import ProductPageComponent from '../components/ProductPageComponent'
 
-export default function ProductPage(){
-    return(
-        <ProductPageComponent />
+export default function ProductPage() {
+    return (
+        <main>
+            <ProductPageComponent />
+            <h5 className="moreProductsRelation">Productos Relacionados</h5>
+
+        </main>
     )
 }
